fbd58af2f76ed65d1ac587cb1f6503dfd97d286a
Peter Gillett <peter@gillett.co.uk>
2022-12-21 23:01:38 +0000

    Day 20 tidy

--- Advent/Advent/Day20.cs
+++ Advent/Advent/Day20.cs
@@ -6,144 +6,45 @@ namespace Advent;
 
 public class Day20
 {
-    public long Method2(string input)
+    public long Method2(string input) => Mix(input, 811589153L, 10);
+
+    public long Method(string input) => Mix(input, 1, 1);
+
+    public long Mix(string input, long encryption, int number)
     {
         var list = input.Split(Environment.NewLine)
-            .Select(l => new Item(int.Parse(l) * 811589153L)).ToList();
-
+            .Select(l => new Item(int.Parse(l) * encryption)).ToList();
+        
         var process = list.ToList();
-        for (var pass = 0; pass < 10; pass++)
+
+        for (var pass = 0; pass < number; pass++)
         {
             foreach (var item in list)
             {
-                var i = process.IndexOf(item);
-                for (var m = 0; m < Math.Abs(item.Value) % (list.Count - 1); m++)
+                var pos = process.IndexOf(item);
+                for (var move = 0; move < Math.Abs(item.Value) % (list.Count - 1); move++)
                 {
-                    if (item.Value > 0)
-                    {
-                        var t = i + 1;
-                        var x = Mod(t, list.Count);
-                        var y = Mod(i, list.Count);
-                        (process[x], process[y]) =
-                            (process[y], process[x]);
-                        i++;
-                    }
-                    else
-                    {
-                        var t = i - 1;
-                        var x = Mod(t, list.Count);
-                        var y = Mod(i, list.Count);
-                        (process[x], process[y]) =
-                            (process[y], process[x]);
-                        i--;
-                    }
+                    var first = Mod(pos, list.Count);
+                    var direction = item.Value > 0 ? 1 : -1;
+                    var second = Mod(pos + direction, list.Count);
+                    pos += direction;
+                    (process[second], process[first]) = (process[first], process[second]);
                 }
-
-            }
-
-
-            Console.WriteLine();
-            foreach (var p in process)
-            {
-                Console.Write(p.Value + " ");
             }
         }
 
-        var find = process.Single(i => i.Value == 0);
-        var start = process.IndexOf(find);
-        var r1 = process[Mod(start + 1000, list.Count)].Value;
-        var r2 = process[Mod(start + 2000, list.Count)].Value;
-        var r3 = process[Mod(start + 3000, list.Count)].Value;
-        
-        return r1 + r2 + r3;
+        return Calc(process, list);
     }
-    public long Method(string input)
+    
+    private long Calc(List<Item> process, List<Item> list)
     {
-   //     var list = input.Split(Environment.NewLine)
-     //       .Select(v => new Link(int.Parse(v)))
-      //      .ToArray();
-         var list = input.Split(Environment.NewLine)
-             .Select(l => new Item(int.Parse(l))).ToList();
-
-        var process = list.ToList();
-
-         // for (var l = 0; l < list.Length; l++)
-         // {
-         //     if (l > 1) list[l].Before = list[l - 1];
-         //     if (l < list.Length - 1) list[l].After = list[l + 1];
-         // }
-         //
-         // list[0].Before = list[list.Length - 1];
-         // list[list.Length - 1].After = list[0];
-
-//        var pos = list[0];
-
-        foreach (var item in list)
-        {
-            var i = process.IndexOf(item);
-            for (var m = 0; m < Math.Abs(item.Value); m++)
-            {
-                if (item.Value > 0)
-                {
-                    var t = i + 1;
-                    var x = Mod(t, list.Count);
-                    var y = Mod(i, list.Count);
-                    (process[x], process[y]) = 
-                        (process[y], process[x]);
-                    i++;
-                }
-                else
-                {
-                    var t = i - 1;
-                    var x = Mod(t, list.Count);
-                    var y = Mod(i, list.Count);
-                    (process[x], process[y]) = 
-                        (process[y], process[x]);
-                    i--;
-                }
-            }
-        
-            // Console.WriteLine();
-            // foreach(var p in process)
-            // {
-            //       Console.Write(p.Value+" ");
-            // }
-        }
-
         var find = process.Single(i => i.Value == 0);
         var start = process.IndexOf(find);
-        var r1 = process[Mod(start + 1000, list.Count)].Value;
-        var r2 = process[Mod(start + 2000, list.Count)].Value;
-        var r3 = process[Mod(start + 3000, list.Count)].Value;
-        
+        var r1 = process[(start + 1000) % list.Count].Value;
+        var r2 = process[(start + 2000) % list.Count].Value;
+        var r3 = process[(start + 3000) % list.Count].Value;
+
         return r1 + r2 + r3;
-        
-        
-
-        
-
-        // foreach (var item in list)
-        // {
-        //     var swap = item;
-        //     for(var i = 0; i< Math.Abs(item.Value); i++)
-        //     if (item.Value > 0)
-        //     {
-        //         for (var i = 0; i < item.Value; i++)
-        //         {
-        //             var a = item.After;
-        //             var b = item.Before;
-        //             b.After = a;
-        //             a.Before = b;
-        //             item.Before = a;
-        //             item.After = a.After;
-        //             a.After.Before = item;
-        //             a.After = item;
-        //         }
-        //     }
-        //     
-        // }
-
-        return 0;
     }
 
     public class Item
@@ -156,16 +57,4 @@ public class Day20
     }
     
     public int Mod(int i, int m) => (i % m + m) % m;
-
-    public class Link
-    {
-        public Link Before;
-        public Link After;
-        public int Value;
-
-        public Link(int value)
-        {
-            Value = value;
-        }
-    }
 }
